a388b84bb54fa9321838bfcef487172b7c18c54e
httpassoca <me@passoca.dev>
2022-06-17 04:34:36 +0100

    feat: add label in apps and roboto flex

--- src/lib/components/App.svelte
+++ src/lib/components/App.svelte
@@ -1,14 +1,41 @@
 <script lang="ts">
   import type { App } from "$lib/types";
   export let app: App;
+  export let active = false;
 </script>
 
-<div class="app" style="background-image: url(/assets/apps/{app.coverImage})" />
+<div class="app" class:active>
+  <div
+    class="image"
+    style="background-image: url(/assets/apps/{app.coverImage})"
+  />
+  {#if active}<div class="label">Start</div>{/if}
+</div>
 
 <style lang="sass">
 .app
   height: 200px
   width: 200px
-  background-size: cover
-  background-position: center
+  display: flex
+  flex-direction: column
+  align-items: center
+  .image
+    height: 100%
+    width: 100%
+    background-size: cover
+    background-position: center
+  &.active
+    width: 344px
+    height: unset
+    padding: 2px
+    border: 5px solid #DED6E2
+    margin: 0px 3px
+    .image
+      border: 4px solid #95A7C7
+      height: 330px
+    .label
+      padding: 19px 0
+      font-size: 32px
+      font-weight: 300
+
 </style>
--- src/lib/components/Apps.svelte
+++ src/lib/components/Apps.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   import App from "./App.svelte";
 
+  let active = "Spider-Man";
+
   const apps = [
     {
       name: "Bloodborne",
@@ -10,19 +12,52 @@
       name: "Spider-Man",
       coverImage: "spider-man.webp",
     },
+    {
+      name: "Bloodborne1",
+      coverImage: "bloodborne.webp",
+    },
+    {
+      name: "Spider-Man2",
+      coverImage: "spider-man.webp",
+    },
+    {
+      name: "Bloodborne3",
+      coverImage: "bloodborne.webp",
+    },
+    {
+      name: "Spider-Man4",
+      coverImage: "spider-man.webp",
+    },
+    {
+      name: "Bloodborne5",
+      coverImage: "bloodborne.webp",
+    },
+    {
+      name: "Spider-Man6",
+      coverImage: "spider-man.webp",
+    },
+    {
+      name: "Bloodborne7",
+      coverImage: "bloodborne.webp",
+    },
+    {
+      name: "Spider-Man8",
+      coverImage: "spider-man.webp",
+    },
   ];
 </script>
 
 <div class="apps">
   {#each apps as app (app.name)}
-    <App {app} />
+    <App {app} active={active === app.name} />
   {/each}
 </div>
 
 <style lang="sass">
 .apps
   display: flex
-  gap: 2px
+  gap: 4px
   margin-top: 210px
   margin-left: 73px
+  width: fit-content
 </style>
