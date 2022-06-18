9a2ee9e2c4a2286924552c177957f45d02500cb9
httpassoca <me@passoca.dev>
2022-06-18 08:50:32 +0100

    fix alias

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
+  plugins: [svelte()],
+  resolve: {
+    alias: {
+      '$lib/': new URL('./src/lib/', import.meta.url).pathname,
+    },
+  },
 })
